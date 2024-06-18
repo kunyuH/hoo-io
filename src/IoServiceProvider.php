0c8579f4fac12fd48928b8a2ad2910f950a980b4
洪坤宇 <1960049949@qq.com>
2024-06-18 13:50:30 +0800

    修复数据库查询服务注册问题

--- src/IoServiceProvider.php
+++ src/IoServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace hoo\io;
-use io\database\services\BuilderMacroSql;
+use hoo\io\database\services\BuilderMacroSql;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
@@ -9,7 +9,7 @@ class IoServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //注册
+        //注册 sql查询服务
         QueryBuilder::macro('getSqlQuery', function () {
             return (new BuilderMacroSql())->getSqlQuery($this);
         });
