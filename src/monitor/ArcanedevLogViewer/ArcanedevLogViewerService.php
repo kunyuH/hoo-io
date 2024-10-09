de3821651b3665311ed404be9afd0b120b62aef4
洪坤宇 <1960049949@qq.com>
2024-10-09 15:22:46 +0800

    增加编排

--- src/monitor/ArcanedevLogViewer/ArcanedevLogViewerService.php
+++ src/monitor/ArcanedevLogViewer/ArcanedevLogViewerService.php
@@ -8,14 +8,21 @@ class ArcanedevLogViewerService
 {
     /**
      * 日志view页面 静态资源链接替换，防止无法访问
+     * 以及跳转链接修正
      */
     public function replaceStaticResourceLink(Request $request,$response,$reqPath)
     {
         # log-viewer
         if(ho_fnmatchs(config('log-viewer.route.attributes.prefix','log-viewer').'/*',$reqPath)) {
             $html = $response->getContent();
+
+            $html = str_replace('/'.config('log-viewer.route.attributes.prefix','log-viewer').'/',
+                jump_link('/'.config('log-viewer.route.attributes.prefix','log-viewer').'/'), $html);
+
             # 字符串替换
             $html = str_replace('https://maxcdn.bootstrapcdn.com', get_cdn() . '/log-viewer', $html);
+
+
             $response->setContent($html);
             return $response;
         }
